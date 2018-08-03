0284f7b710dc724d2ce8f0a8ae6b844a1e9b6be2
tummy <guitum@msn.com>
2018-08-03 17:30:41 +0700

    Code Behide

--- WebIBOST1/SummaryDashboard.aspx.cs
+++ WebIBOST1/SummaryDashboard.aspx.cs
@@ -4,41 +4,85 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using System.Reflection;
 namespace WebIBOST1
 {
     public partial class SummaryDashboard : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            IBOrderTrackingEntities oConnect = new IBOrderTrackingEntities();
-            grdworking.DataSource = oConnect.SOHeaders.ToList();
-            grdworking.DataBind();
+            GenerateTable();
+            //IBOrderTrackingEntities oConnect = new IBOrderTrackingEntities();
+            //grdworking.DataSource = oConnect.SOHeaders.ToList();
+            //grdworking.DataBind();
         }
+
         private void GenerateTable()
         {
             //Header
+            //Add header to table
             TableRow oRow = new TableRow();
             oRow.TableSection = TableRowSection.TableHeader;
 
-            TableCell oCell = new TableCell();
-            oCell.Text = "SO";
-            //add header to table
-            oRow.Cells.Add(oCell);
-
-            TableCell oCell1 = new TableCell();
-            oCell1.Text = "DO";
-            oRow.Cells.Add(oCell1);
-
-            TableCell oCell2 = new TableCell();
-            oCell2.Text = "loading";
-            oRow.Cells.Add(oCell2);
-
-            //finish 
+            int noCol = 0;
+            foreach (var item in getSOHeader())
+            {
+                if (noCol < 9)
+                {
+                    TableCell oH1 = new TableCell();
+                    oH1.Text = item;
+                    oRow.Cells.Add(oH1);
+                }
+                noCol++;
+            }
+            //Finish Header
             tblData.Rows.Add(oRow);
 
-            //add
-            tblData.Attributes.Add("class","table table - striped table - bordered table - hover");
+            //Rows
+            WebIBOST1.IBOrderTrackingEntities oConnect = new IBOrderTrackingEntities();
+            var soItem = oConnect.SOHeaders.ToList();
+
+            foreach (var row in soItem)
+            {
+                noCol = 0;
+                //Add Detail to table
+                TableRow oDetail = new TableRow();
+                oDetail.TableSection = TableRowSection.TableBody;
+
+                Type oType = row.GetType();
+                PropertyInfo[] props = oType.GetProperties();
+                foreach (var prop in props)
+                {
+                    //Row 1 Col 1
+                    if (noCol < 9)
+                    {
+                        TableCell oR1 = new TableCell();
+                        oR1.Text = prop.GetValue(row) != null ? prop.GetValue(row).ToString() : "";
+                        oDetail.Cells.Add(oR1);
+                    }
+                    noCol++;
+                }
+                // Finish 1 Row
+                tblData.Rows.Add(oDetail);
+            }
+
+
+            // Add class
+            tblData.Attributes.Add("class", "table table-striped table-bordered table-hover");
+
+        }
+
+        private List<String> getSOHeader()
+        {
+            List<String> oResult = new List<string>();
+
+            Type soType = typeof(SOHeader);
+
+            foreach (var item in soType.GetProperties())
+            {
+                oResult.Add(item.Name);
+            }
+            return oResult;
         }
     }
 }
